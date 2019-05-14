e80bfd6a2e2cecebd58f7b13679a503e438b082d
kimkaram <47126446+kimkaram@users.noreply.github.com>
2019-05-14 15:52:54 +0900

    Create number3.java
    p19 3번 문제

---
+++ chapter1/number3.java
@@ -0,0 +1,35 @@
+package chapter1;
+
+import java.util.Scanner;
+
+public class P19_3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("a : ");
+		int a = sc.nextInt();
+		System.out.print("b : ");
+		int b = sc.nextInt();
+		System.out.print("c : ");
+		int c = sc.nextInt();
+		
+		max(a, b, c);
+
+	}
+	
+	public static int max(int a, int b, int c) {
+		
+		int max = a;
+		
+		if(b > max)
+			max = b;
+		if(c > max)
+			max = c;
+		
+		System.out.println("max : " + max);
+		return max;
+		
+	}
+
+}
